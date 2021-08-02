68a7301dc18414bbc2a5326b4bd4f9d3d1c4ae68
Angelo Carvalho <angelosuporteti@gmail.com>
2021-08-02 13:41:54 -0300

    Same examples of concatenation

--- app_pyLearning/operationBasic.py
+++ app_pyLearning/operationBasic.py
@@ -6,12 +6,15 @@ multiplicacao = a * b
 divisao = a / b
 resto = a % b
 
+print('Soma : {}'.format(soma))
+print('Soma : {}. Subtracao: {}'.format(soma, subtracao))
+print('Soma : {soma}. Subtracao: {subtracao}'.format(soma = soma, subtracao = subtracao))
 print('Soma: ' + str(soma))
 print(subtracao)
 print(multiplicacao)
 print(divisao)
 print(resto)
 
-x = '1'
-soma2 = int(x) + 1
-print(soma2)
+# x = '1'
+# soma2 = int(x) + 1
+# print(soma2)
