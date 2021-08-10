bdbafee4ba73f12e375324228fc0e05582edaf7e
Angelo Carvalho <angelosuporteti@gmail.com>
2021-08-09 23:46:20 -0300

    Show ip of network

--- pyLearningSecurity/ips.py
+++ pyLearningSecurity/ips.py
@@ -1,6 +1,6 @@
 import ipaddress
 
-ip = '192.168.0.1'
+ip = '192.168.0.0/24'
 
-endereco = ipaddress.ip_address(ip)
-print(endereco + 2000)
+rede = ipaddress.ip_network(ip, strict=False)
+print(rede)
