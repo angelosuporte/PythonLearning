222031287c1b581f4edc206d786e7203f31ade5b
Angelo Carvalho <angelosuporteti@gmail.com>
2021-08-10 23:43:19 -0300

    With option hide directory

--- pyLearningSecurity/hideFiles.py
+++ pyLearningSecurity/hideFiles.py
@@ -1,8 +1,10 @@
 import ctypes
 
+diretorio = input('Informe o caminho do diretório a ser ocultado: ')
 atributo_ocultar = 0x02
 
-retorno = ctypes.windll.kernel32.SetFileAttributesW('ocultar.txt', atributo_ocultar)
+retorno = ctypes.windll.kernel32.SetFileAttributesW(diretorio, atributo_ocultar)
+#retorno = ctypes.windll.kernel32.SetFileAttributesW('ocultar.txt', atributo_ocultar)
 
 if retorno:
     print('Arquivo foi ocultado')
